c9887b90825f014d5f0181eb9cf2b3d19ec4a66c
08sm80 <code@dailyascent.net>
2024-09-19 18:09:10 +0530

    update provider config

--- provider.tf
+++ provider.tf
@@ -1,1 +1,13 @@
+provider "google" { # google provider
+  project     = "learn-tf-code"
+  region      = "us-central1"
+  credentials = "/d/Code/keys/learn-tf-code-84e45c1a1298.json"
+}
 
+terraform { # terraform provider for GCP
+  required_providers {
+    google = {
+      version = "~> 4.0.0"
+    }
+  }
+}
